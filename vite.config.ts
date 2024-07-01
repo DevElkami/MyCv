ad4fe0631211132c6eb0c550d166f81e70310ed0
Elkami <49388297+DevElkami@users.noreply.github.com>
2024-07-01 17:18:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,31 @@ export default defineConfig({
                   proxy.on("error", (err, _req, _res) => {
                     console.log("proxy error", err);
                   });
+                  proxy.on("proxyReq", async (proxyReq, req, _res) => {
+             
+                    const requestBody = await getPostData(req);
+                    console.log("requestBody", requestBody);
+                    console.log(
+                      "Sending Request:",
+                      req.method,
+                      req.url,
+                      " => TO THE TARGET =>  ",
+                      proxyReq.method,
+                      proxyReq.protocol,
+                      proxyReq.host,
+                      proxyReq.path,
+                      JSON.stringify(proxyReq.getHeaders()),
+                    );
+                  });
+                  proxy.on("proxyRes", (proxyRes, req, _res) => {
+                    console.log(
+                      "Received Response from the Target:",
+                      proxyRes.statusCode,
+                      req.url,
+                      JSON.stringify(proxyRes.headers),
+                    );
+                  });
+                },
             },
             "^/climb/.*": {
                 target: "https://climb.elkami.fr/feed",
