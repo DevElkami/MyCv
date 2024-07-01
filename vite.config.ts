b259544b4228f534ec631165b2e9d0965fbb880a
Elkami <49388297+DevElkami@users.noreply.github.com>
2024-07-01 16:56:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({    
     server: {        
         proxy: {
-            "/blog": {
+            "^/blog": {
                 target: "https://blog.elkami.fr/feed",
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path = "https://blog.elkami.fr/feed",
             },
-            "/climb": {
+            "^/climb": {
                 target: "https://climb.elkami.fr/feed",
                 changeOrigin: true,
                 secure: false,
