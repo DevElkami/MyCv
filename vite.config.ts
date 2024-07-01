4572e9e18a0ccce3fd610e55d5a32eeaad7c0660
Elkami <49388297+DevElkami@users.noreply.github.com>
2024-07-01 16:43:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({    
-    server: {
-        cors: {
-            origin: 'https://blog.elkami.fr/feed',
-            credentials: false,
-        },
+    server: {        
         proxy: {
-            "/blog": {
+            "https://develkami.github.io/blog": {
                 target: "https://blog.elkami.fr/feed",
                 changeOrigin: true,
                 secure: false,
