893b7a4b50299b6cc67c2bc43bb5eb118f267754
DevElkami <m.reignier@cyberjet.fr>
2024-08-07 11:03:03 +0200

    Optimized height

--- src/component/skills.tsx
+++ src/component/skills.tsx
@@ -1,46 +1,48 @@
-import { Stack, Typography, LinearProgress, Box, Card, CardContent } from "@mui/material";
+import { Stack, Typography, LinearProgress, Box, Card, CardContent, Grid } from "@mui/material";
 function Skills() {
     return (
         <Card>
             <CardContent>
                 <Stack direction="column" spacing={ 1 }>
                     <Typography variant="h5" component="div" align="left">Skills</Typography>
-                    <Box>
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">C#</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={90} />
                         </Stack>
-                    </Box>
-                    <Box>
+                    </Grid>
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">ASP.NET Core</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={90} />
                         </Stack>
-                    </Box>
-                    <Box>
+                    </Grid>
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">Dev Ops</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={85} />
                         </Stack>
-                    </Box>
-                    <Box>
+                    </Grid>
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">SQL</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={80} />
                         </Stack>
-                    </Box>                    
-                    <Box>
+                    </Grid>                    
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">C++</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={75} />
                         </Stack>
-                    </Box>
-                    <Box>
+                    </Grid>
+                    <Grid item xs={6}>
                         <Stack spacing={{ xs: 1 }}>
                             <Typography align="left" variant="subtitle1" color="text.secondary">React</Typography>
                             <LinearProgress color="secondary" variant="determinate" value={60} />
                         </Stack>
-                    </Box>          
+                    </Grid>
+                    </Grid>      
                 </Stack>
             </CardContent>
         </Card>
